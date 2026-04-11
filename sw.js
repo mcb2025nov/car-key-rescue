62b37b6f8c2b0d4e39e07758c5cf2366fe925dbf
mcb2025nov <cmbrandon_pe@yahoo.com>
2026-04-10 21:50:06 -0400

    Add PWA support: manifest, service worker, and app icons

---
+++ sw.js
@@ -0,0 +1,43 @@
+const CACHE_NAME = 'ckr-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './logo.jpg',
+  './icon-192.png',
+  './icon-512.png',
+  './manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      return (
+        cached ||
+        fetch(event.request)
+          .then((resp) => {
+            const copy = resp.clone();
+            caches.open(CACHE_NAME).then((c) => c.put(event.request, copy));
+            return resp;
+          })
+          .catch(() => cached)
+      );
+    })
+  );
+});
